08972034bd7786e7c5e8616de0a3e2f024708378
Renluyue <2697753132@qq.com>
2019-11-26 10:31:23 +0800

    ffff

---
+++ 1126zuoye.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(void)
+{
+    int i,m;
+    int count=0;
+    for(m=2;m<100;m++)
+    {
+    for(i=2;i<=m/2;i++)
+    if(m%i==0)
+    break;
+    if(i>m/2)
+    {
+    count++;
+    printf("%-3d",m);
+    if(count%5==0)
+    printf("\n");
+    }
+    }
+    return 0;
+}
